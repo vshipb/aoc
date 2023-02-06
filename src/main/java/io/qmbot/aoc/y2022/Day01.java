5c4c5d4709b373ed55818e039d6fdb72bfd54e45
QmBotox <122793467+QmBotox@users.noreply.github.com>
2023-02-06 09:49:22 +0200

    Day2 (#4)
    * ReadMeUpgrade
    
    * ReadMeUpgrade
    
    * Day01
    
    * Day01
    
    * Day02
    
    * Day03
    
    ---------
    
    Co-authored-by: vshipb <valery.shorin@smddev.com>

--- src/main/java/io/qmbot/aoc/y2022/Day01.java
+++ src/main/java/io/qmbot/aoc/y2022/Day01.java
@@ -5,11 +5,11 @@ import io.qmbot.aoc.Puzzle;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class Day01 implements Puzzle {
-    private static String PATH = "C:\\Users\\arina\\IdeaProjects\\untitled2\\src\\main\\resources\\2022_1.txt";
+    private static String PATH = "C:\\Users\\arina\\IdeaProjects\\untitled2\\src\\main\\resources\\2022_1.1.txt";
 
     public static void main(String[] args) throws IOException {
         Puzzle puzzle = new Day01();
@@ -19,46 +19,58 @@ public class Day01 implements Puzzle {
     }
     @Override
     public String part1(String input) {
-        int elvCaloriesMax = 0;
-        int elv = 0;
-        for (String line : input.lines().toList()) {
-            if (line.isEmpty()) {
-                if (elv > elvCaloriesMax) {
-                    elvCaloriesMax = elv;
-                }
-                elv = 0;
-            } else {
-                elv = elv + Integer.parseInt(line);
-            }
-        }
-        return Integer.toString(elvCaloriesMax);
+//        int elvCaloriesMax = 0;
+//        int elv = 0;
+//        for (String line : input.lines().toList()) {
+//            if (line.isEmpty()) {
+//                if (elv > elvCaloriesMax) {
+//                    elvCaloriesMax = elv;
+//                }
+//                elv = 0;
+//            } else {
+//                elv = elv + Integer.parseInt(line);
+//            }
+//        }
+//        System.out.println(elvCaloriesMax);
+
+        return Long.toString(Arrays.stream(input.split("\r\n\r\n"))
+                .mapToLong(s -> s.lines().mapToLong(Long::parseLong).sum())
+                .max().orElseThrow());
     }
+
 
     @Override
     public String part2(String input) {
-        int elv1 = 0;
-        int elv2 = 0;
-        int elv3 = 0;
-        int elv = 0;
-        for (String line : input.lines().toList()) {
-            if (line.isEmpty()) {
-                if (elv > elv1) {
-                    elv3 = elv2;
-                    elv2 = elv1;
-                    elv1 = elv;
-                } else if (elv > elv2) {
-                    elv3 = elv2;
-                    elv2 = elv;
-                }
-                elv = 0;
-            } else {
-                elv = elv + Integer.parseInt(line);
-                if ((elv > elv3) & (elv < elv2)) {
-                    elv3 = elv;
-                }
-            }
-        }
-        int sum = elv2 + elv1 + elv3;
-        return Integer.toString(sum);
+
+//        int elv1 = 0;
+//        int elv2 = 0;
+//        int elv3 = 0;
+//        int elv = 0;
+//        for (String line : input.lines().toList()) {
+//            if (line.isEmpty()) {
+//                if (elv > elv1) {
+//                    elv3 = elv2;
+//                    elv2 = elv1;
+//                    elv1 = elv;
+//                } else if (elv > elv2) {
+//                    elv3 = elv2;
+//                    elv2 = elv;
+//                }
+//                elv = 0;
+//            } else {
+//                elv = elv + Integer.parseInt(line);
+//                if ((elv > elv3) & (elv < elv2)) {
+//                    elv3 = elv;
+//                }
+//            }
+//        }
+//        int sum = elv2 + elv1 + elv3;
+//        System.out.println(sum);
+
+        long[] sort = Arrays.stream(input.split("\r\n\r\n"))
+                .mapToLong(s -> s.lines().mapToLong(Long::parseLong).sum())
+                .sorted().toArray();
+        int length = sort.length;
+        return Long.toString(sort[length-1]+sort[length-2]+sort[length-3]);
     }
 }
