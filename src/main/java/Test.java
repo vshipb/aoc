8eaaf6c3b73a2e960a724be20d5c8bbef7c3d5fd
vshipb <vsh.ipb@gmail.com>
2023-02-07 11:47:02 +0300

    checkstyle added (#5)
    * checkstyle added
    
    * checkstyle added
    
    * checkstyle added
    
    * checkstyle added
    
    ---------
    
    Co-authored-by: Valery Shorin <valery.shorin@wandisco.com>

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,6 +1,6 @@
 public class Test {
 
-  public static void main(String... args) {
-    System.out.println("Hello world");
-  }
+    public static void main(String... args) {
+        System.out.println("Hello world");
+    }
 }
--- src/main/java/io/qmbot/aoc/Puzzle.java
+++ src/main/java/io/qmbot/aoc/Puzzle.java
@@ -1,6 +1,7 @@
 package io.qmbot.aoc;
 
 public interface Puzzle {
-  String part1(String input);
-  String part2(String input);
+    String part1(String input);
+
+    String part2(String input);
 }
