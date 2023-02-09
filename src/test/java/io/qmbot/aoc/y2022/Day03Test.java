88c3411ec5a6877e03ab9e623f41b5770db3c7ff
QmBotox <arina.shorina@gmail.com>
2023-02-09 20:27:01 +0200

    Day03Test

---
+++ src/test/java/io/qmbot/aoc/y2022/Day03Test.java
@@ -0,0 +1,26 @@
+package io.qmbot.aoc.y2022;
+
+import io.qmbot.aoc.Puzzle;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Day03Test {
+    Puzzle p = new Day03();
+
+    String input = "vJrwpWtwJgWrhcsFMMfFFhFp\n" +
+            "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL\n" +
+            "PmmdzqPrVvPwwTWBwg\n" +
+            "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn\n" +
+            "ttgJtRGJQctTZtZT\n" +
+            "CrZsJsPPZsGzwwsLwLmpwMDw";
+
+    @Test
+    public void part1(){
+        Assertions.assertEquals("157", p.part1(input));
+    }
+
+    @Test
+    public void part2(){
+        Assertions.assertEquals("70", p.part2(input));
+    }
+}
