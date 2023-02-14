45b93cb0921c10bc480356fe55d784e3101d1617
QmBotox <arina.shorina@gmail.com>
2023-02-14 14:45:10 +0200

    Day04 + Test

---
+++ src/test/java/io/qmbot/aoc/y2022/Day04Test.java
@@ -0,0 +1,26 @@
+package io.qmbot.aoc.y2022;
+
+import io.qmbot.aoc.Puzzle;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Day04Test {
+    Puzzle p = new Day04();
+
+    String input = "2-4,6-8\n" +
+            "2-3,4-5\n" +
+            "5-7,7-9\n" +
+            "2-8,3-7\n" +
+            "6-6,4-6\n" +
+            "2-6,4-8";
+
+    @Test
+    public void part1(){
+        Assertions.assertEquals("2", p.part1(input));
+    }
+
+    @Test
+    public void part2(){
+        Assertions.assertEquals("4", p.part2(input));
+    }
+}
