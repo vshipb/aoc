eeafdaf80dda5eb8ec331b605b035a805e024399
QmBotox <122793467+QmBotox@users.noreply.github.com>
2023-03-09 10:33:12 +0200

    Day08 (#19)
    * Day04
    
    * .gitignore
    
    * Day08

---
+++ src/test/java/io/qmbot/aoc/y2022/Day08Test.java
@@ -0,0 +1,25 @@
+package io.qmbot.aoc.y2022;
+
+import io.qmbot.aoc.Puzzle;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Day08Test {
+    Puzzle p = new Day08();
+
+    String input = "30373\n" +
+            "25512\n" +
+            "65332\n" +
+            "33549\n" +
+            "35390";
+
+    @Test
+    public void part1(){
+        Assertions.assertEquals("21", p.part1(input));
+    }
+
+    @Test
+    public void part2(){
+        Assertions.assertEquals("8", p.part2(input));
+    }
+}
