13146aa7a4f90f4ac415e75c691b7e0f7150ce53
QmBotox <arina.shorina@gmail.com>
2023-03-16 09:49:45 +0200

    Day12

---
+++ src/test/java/io/qmbot/aoc/y2022/Day12Test.java
@@ -0,0 +1,25 @@
+package io.qmbot.aoc.y2022;
+
+import io.qmbot.aoc.Puzzle;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Day12Test {
+    Puzzle p = new Day12();
+
+    String input = "Sabqponm\n" +
+            "abcryxxl\n" +
+            "accszExk\n" +
+            "acctuvwj\n" +
+            "abdefghi";
+
+    @Test
+    public void part1(){
+        Assertions.assertEquals("31", p.part1(input));
+    }
+
+    @Test
+    public void part2(){
+        Assertions.assertEquals("29", p.part2(input));
+    }
+}
