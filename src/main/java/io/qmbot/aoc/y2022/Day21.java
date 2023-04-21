fc2dc6947d2dc544065344c188c8a3e3382754a9
QmBotox <arina.shorina@gmail.com>
2023-04-21 11:50:30 +0300

    Day17 + Day18

--- src/main/java/io/qmbot/aoc/y2022/Day21.java
+++ src/main/java/io/qmbot/aoc/y2022/Day21.java
@@ -1,127 +1,119 @@
 package io.qmbot.aoc.y2022;
 
 import io.qmbot.aoc.Puzzle;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Day21 implements Puzzle {
     @Override
     public String part1(String input) {
-        Map<String, Long> monkeyWithNumber = new HashMap<>();
-        Map<String, String[]> monkeyWithOperation = new HashMap<>();
-
-        for (String string : input.split("\n")) {
-            String[] stringSplit = string.split(" ");
-            String monkeyName = stringSplit[0].replaceFirst(":", "");
-            if (stringSplit.length == 2) {
-                monkeyWithNumber.put(monkeyName, Long.valueOf(stringSplit[1]));
-            } else {
-                monkeyWithOperation.put(monkeyName, Arrays.copyOfRange(stringSplit, 1, stringSplit.length));
-            }
-        }
-
-        String firstMonkeyName;
-        String secondMonkeyName;
-
-        while (monkeyWithOperation.containsKey("root")) {
-            for (Map.Entry<String, String[]> monkey : monkeyWithOperation.entrySet()) {
-                firstMonkeyName = monkey.getValue()[0];
-                secondMonkeyName = monkey.getValue()[2];
-
-                if (monkeyWithNumber.containsKey(firstMonkeyName) && monkeyWithNumber.containsKey(secondMonkeyName)) {
-                    monkeyWithNumber.put(monkey.getKey(), newNumberMonkeyValue(monkeyWithNumber.get(firstMonkeyName),
-                            monkeyWithNumber.get(secondMonkeyName), monkey.getValue()[1]));
-                    monkeyWithOperation.remove(monkey.getKey());
-
-                    break;
-                }
-            }
-        }
-
-        return String.valueOf(monkeyWithNumber.get("root"));
+        Map<String, Monkey> monkeys = monkeys(input);
+        return String.valueOf(monkeys.get("root").yell(monkeys));
     }
 
     @Override
     public String part2(String input) {
-        Map<String, Long> monkeyWithNumber = new HashMap<>();
-        Map<String, String[]> monkeyWithOperation = new HashMap<>();
-        String firstMonkeyName;
-        String secondMonkeyName;
         long low = -(long) Integer.MAX_VALUE * 1024 * 1024;
         long high = (long) Integer.MAX_VALUE * 1024 * 1024;
         long humn;
+        long root;
+
+        Map<String, Monkey> monkeys = monkeys(input);
+        ((MonkeyWithOperation) monkeys.get("root")).operation = "-";
+
+        long lowValue = root(monkeys, low);
+        long highValue = root(monkeys, high);
+
+        if (Long.signum(lowValue) == Long.signum(highValue)) {
+            throw new IllegalStateException();
+        }
 
         while (true) {
-            for (String string : input.split("\n")) {
-                String[] stringSplit = string.split(" ");
-                String monkeyName = stringSplit[0].replaceFirst(":", "");
-                if (stringSplit.length == 2) {
-                    monkeyWithNumber.put(monkeyName, Long.valueOf(stringSplit[1]));
-                } else {
-                    if (monkeyName.equals("root")) {
-                        monkeyWithOperation.put(monkeyName, new String[]{stringSplit[1], "=", stringSplit[3]});
-                    } else {
-                        monkeyWithOperation.put(monkeyName, Arrays.copyOfRange(stringSplit, 1, stringSplit.length));
-                    }
-                }
-            }
-
-            monkeyWithNumber.remove("humn");
-
             humn = low + (high - low) / 2;
+            root = root(monkeys, humn);
 
-            monkeyWithNumber.put("humn", humn);
-
-            while (monkeyWithOperation.containsKey("root")) {
-                for (Map.Entry<String, String[]> monkey : monkeyWithOperation.entrySet()) {
-                    firstMonkeyName = monkey.getValue()[0];
-                    secondMonkeyName = monkey.getValue()[2];
-
-                    if (monkeyWithNumber.containsKey(firstMonkeyName) && monkeyWithNumber.containsKey(secondMonkeyName)) {
-                        monkeyWithNumber.put(monkey.getKey(), newNumberMonkeyValue(monkeyWithNumber.get(firstMonkeyName),
-                                monkeyWithNumber.get(secondMonkeyName), monkey.getValue()[1]));
-                        monkeyWithOperation.remove(monkey.getKey());
-
-                        break;
-                    }
-                }
-            }
-
-            long root = monkeyWithNumber.get("root");
-
-            if (0 < root) {
-                // low = humn + 1;
-                high = humn - 1;
-            } else if (0 > root) {
-                // high = humn - 1;
-                low = humn + 1;
+            if (Long.signum(root) == Long.signum(lowValue)) {
+                low = humn;
+            } else if (Long.signum(root) == Long.signum(highValue)) {
+                high = humn;
             } else {
                 break;
             }
-
-            monkeyWithNumber.clear();
-            monkeyWithOperation.clear();
         }
 
-        return String.valueOf(humn);
+        while (root(monkeys, humn) == 0) {
+            humn--;
+        }
+
+        return String.valueOf(humn + 1);
     }
 
-    private static long newNumberMonkeyValue(long firstMonkeyValue, long secondMonkeyValue, String operation) {
-        switch (operation) {
-            case "-", "=" -> {
-                return firstMonkeyValue - secondMonkeyValue;
+    private static Map<String, Monkey> monkeys(String input) {
+        Map<String, Monkey> monkeys = new HashMap<>();
+        for (String string : input.split("\n")) {
+            String[] stringSplit = string.split(": ");
+            String monkeyName = stringSplit[0];
+            String[] value = stringSplit[1].split(" ");
+            if (value.length == 1) {
+                monkeys.put(monkeyName, new MonkeyWithNumber(Long.parseLong(value[0])));
+            } else {
+                monkeys.put(monkeyName, new MonkeyWithOperation(value[0], value[1], value[2]));
             }
-            case "+" -> {
-                return firstMonkeyValue + secondMonkeyValue;
+        }
+        return monkeys;
+    }
+
+    private static long root(Map<String, Monkey> monkeys, long humn) {
+        ((MonkeyWithNumber) monkeys.get("humn")).number = humn;
+
+        return monkeys.get("root").yell(monkeys);
+    }
+
+    interface Monkey {
+        long yell(Map<String, Monkey> monkeys);
+    }
+
+    private static class MonkeyWithNumber implements Monkey {
+        long number;
+
+        public MonkeyWithNumber(long number) {
+            this.number = number;
+        }
+
+        @Override
+        public long yell(Map<String, Monkey> monkeys) {
+            return number;
+        }
+    }
+
+    private static class MonkeyWithOperation implements Monkey {
+        String first;
+        String second;
+        String operation;
+
+        public MonkeyWithOperation(String first, String operation, String second) {
+            this.first = first;
+            this.operation = operation;
+            this.second = second;
+        }
+
+        @Override
+        public long yell(Map<String, Monkey> monkeys) {
+            switch (operation) {
+                case "-" -> {
+                    return monkeys.get(first).yell(monkeys) - monkeys.get(second).yell(monkeys);
+                }
+                case "+" -> {
+                    return monkeys.get(first).yell(monkeys) + monkeys.get(second).yell(monkeys);
+                }
+                case "*" -> {
+                    return monkeys.get(first).yell(monkeys) * monkeys.get(second).yell(monkeys);
+                }
+                case "/" -> {
+                    return monkeys.get(first).yell(monkeys) / monkeys.get(second).yell(monkeys);
+                }
+                default -> throw new IllegalArgumentException("Operation " + operation + " is not valid");
             }
-            case "*" -> {
-                return firstMonkeyValue * secondMonkeyValue;
-            }
-            case "/" -> {
-                return firstMonkeyValue / secondMonkeyValue;
-            }
-            default -> throw new IllegalArgumentException("Operation " + operation + " is not valid");
         }
     }
 }
