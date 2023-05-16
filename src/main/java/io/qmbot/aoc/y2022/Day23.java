9c852fbfdaf69ea44ef519770970adec3a379b71
QmBotox <arina.shorina@gmail.com>
2023-05-16 11:21:35 +0300

    Day23

--- src/main/java/io/qmbot/aoc/y2022/Day23.java
+++ src/main/java/io/qmbot/aoc/y2022/Day23.java
@@ -4,105 +4,106 @@ import io.qmbot.aoc.Puzzle;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Day23 implements Puzzle {
     @Override
     public String part1(String input) {
         List<Elf> elves = listOfElves(input);
-        int mainDirection = 0;
+        Direction mainDirection = Direction.North;
         for (int i = 0; i < 10; i++) {
             firstHalf(elves, mainDirection);
             secondHalf(elves);
-            mainDirection = nextDirection(mainDirection);
+            mainDirection = mainDirection.nextDirection();
         }
-        int x = elves.stream().mapToInt(elf -> elf.first.x).max().orElseThrow()
-                - elves.stream().mapToInt(elf -> elf.first.x).min().orElseThrow() + 1;
-        int y = elves.stream().mapToInt(elf -> elf.first.y).max().orElseThrow()
-                - elves.stream().mapToInt(elf -> elf.first.y).min().orElseThrow() + 1;
+        int x = elves.stream().mapToInt(elf -> elf.current.x).max().orElseThrow()
+                - elves.stream().mapToInt(elf -> elf.current.x).min().orElseThrow() + 1;
+        int y = elves.stream().mapToInt(elf -> elf.current.y).max().orElseThrow()
+                - elves.stream().mapToInt(elf -> elf.current.y).min().orElseThrow() + 1;
         return String.valueOf((x * y) - elves.size());
     }
 
     @Override
     public String part2(String input) {
         List<Elf> elves = listOfElves(input);
-        int mainDirection = 0;
+        Direction mainDirection = Direction.North;
         int i = 0;
         while (true) {
             i++;
             if (firstHalf(elves, mainDirection)) return String.valueOf(i);
             secondHalf(elves);
-            mainDirection = nextDirection(mainDirection);
-            if (i % 10 == 0) System.out.println(i);
+            mainDirection = mainDirection.nextDirection();
         }
     }
 
-    private static boolean firstHalf(List<Elf> elves, int mainDirection) {
+    private static boolean firstHalf(List<Elf> elves, Direction mainDirection) {
         boolean north;
         boolean south;
         boolean west;
         boolean east;
-        int direction;
+        Direction direction;
         int countNoMove = 0;
+        Set<Point> points = elves.stream().map(e -> e.current).collect(Collectors.toSet());
         for (Elf elf : elves) {
-            north = elf.northIsEmpty(elves);
-            south = elf.southIsEmpty(elves);
-            west = elf.westIsEmpty(elves);
-            east = elf.eastIsEmpty(elves);
+            north = elf.directionIsEmpty(Direction.North, points);
+            south = elf.directionIsEmpty(Direction.South, points);
+            west = elf.directionIsEmpty(Direction.West, points);
+            east = elf.directionIsEmpty(Direction.East, points);
             direction = mainDirection;
             for (int j = 0; j < 4; j++) {
                 if ((north && south && west && east) || (!north && !south && !west && !east)) {
-                    elf.second = elf.first;
+                    elf.next = elf.current;
                     countNoMove++;
                     break;
                 }
                 elfMoves(direction, north, south, west, east, elf);
-                if (elf.second != null) break;
-                direction = nextDirection(direction);
+                if (elf.next != null) break;
+                direction = direction.nextDirection();
             }
         }
         return (countNoMove == elves.size());
     }
 
     private static void secondHalf(List<Elf> elves) {
-        List<Point> duplicates = elves.stream().map(elf -> elf.second)
+        List<Point> duplicates = elves.stream().map(elf -> elf.next)
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting())).entrySet().stream()
                 .filter(p -> p.getValue() > 1).map(Map.Entry::getKey).toList();
         for (Elf elf : elves) {
-            if (!duplicates.contains(elf.second)) {
-                elf.first = elf.second;
+            if (!duplicates.contains(elf.next)) {
+                elf.current = elf.next;
             }
-            elf.second = null;
+            elf.next = null;
         }
     }
 
-    private static void elfMoves(int direction, boolean north, boolean south, boolean west, boolean east, Elf elf) {
-        int y = elf.first.y;
-        int x = elf.first.x;
-        switch (direction) {
-            case 0 -> {
+    private static void elfMoves(Direction dir, boolean north, boolean south, boolean west, boolean east, Elf elf) {
+        Point point = elf.current;
+        switch (dir) {
+            case  North -> {
                 if (north) {
-                    elf.second = new Point(y - 1, x);
+                    elf.next = dir.getPoint(point);
                 }
             }
-            case 1 -> {
+            case South -> {
                 if (south) {
-                    elf.second = new Point(y + 1, x);
+                    elf.next = dir.getPoint(point);
                 }
             }
-            case 2 -> {
+            case West -> {
                 if (west) {
-                    elf.second = new Point(y, x - 1);
+                    elf.next = dir.getPoint(point);
                 }
             }
-            case 3 -> {
+            case East -> {
                 if (east) {
-                    elf.second = new Point(y, x + 1);
+                    elf.next = dir.getPoint(point);
                 }
             }
             default -> throw new
-                    IllegalArgumentException("index of direction " + direction + " is not valid");
+                    IllegalArgumentException("index of direction " + dir + " is not valid");
 
         }
     }
@@ -120,42 +121,69 @@ public class Day23 implements Puzzle {
         return elves;
     }
 
-    private static int nextDirection(int direction) {
-        return  (direction + 1) % 4;
+    interface Dir {
+        Point getPoint(Point p);
+
+        Stream<Point> getPoints(Point p);
+    }
+
+    enum Direction implements Dir {
+        North {
+            @Override
+            public Point getPoint(Point p) {
+                return new Point(p.y - 1, p.x);
+            }
+
+            @Override
+            public Stream<Point> getPoints(Point p) {
+                return Stream.of(new Point(p.y - 1, p.x - 1), new Point(p.y - 1, p.x), new Point(p.y - 1, p.x + 1));
+            }
+        }, South {
+            @Override
+            public Point getPoint(Point p) {
+                return new Point(p.y + 1, p.x);
+            }
+
+            @Override
+            public Stream<Point> getPoints(Point p) {
+                return Stream.of(new Point(p.y + 1, p.x - 1), new Point(p.y + 1, p.x), new Point(p.y + 1, p.x + 1));
+            }
+        }, West {
+            @Override
+            public Point getPoint(Point p) {
+                return new Point(p.y, p.x - 1);
+            }
+
+            @Override
+            public Stream<Point> getPoints(Point p) {
+                return Stream.of(new Point(p.y - 1, p.x - 1), new Point(p.y, p.x - 1), new Point(p.y + 1, p.x - 1));
+            }
+        }, East {
+            @Override
+            public Point getPoint(Point p) {
+                return new Point(p.y, p.x + 1);
+            }
+
+            @Override
+            public Stream<Point> getPoints(Point p) {
+                return Stream.of(new Point(p.y - 1, p.x + 1), new Point(p.y, p.x + 1), new Point(p.y + 1, p.x + 1));
+            }
+        };
+        public Direction nextDirection() {
+            return Direction.values()[(ordinal() + 1) % 4];
+        }
     }
 
     private static class Elf {
-        Point first;
-        Point second;
+        Point current;
+        Point next;
+
         public Elf(int y, int x) {
-            this.first = new Point(y, x);
-        }
-        boolean northIsEmpty(List<Elf> elves) {
-            int north = first.y - 1;
-            int x = first.x;
-            List<Point> points = List.of(new Point(north, x - 1), new Point(north, x), new Point(north, x + 1));
-            return elves.stream().map(e -> e.first).noneMatch(points::contains);
+            this.current = new Point(y, x);
         }
 
-        boolean southIsEmpty(List<Elf> elves) {
-            int south = first.y + 1;
-            int x = first.x;
-            List<Point> points = List.of(new Point(south, x - 1), new Point(south, x), new Point(south, x + 1));
-            return elves.stream().map(e -> e.first).noneMatch(points::contains);
-        }
-
-        boolean westIsEmpty(List<Elf> elves) {
-            int west = first.x - 1;
-            int y = first.y;
-            List<Point> points = List.of(new Point(y - 1, west), new Point(y, west), new Point(y + 1, west));
-            return elves.stream().map(e -> e.first).noneMatch(points::contains);
-        }
-
-        boolean eastIsEmpty(List<Elf> elves) {
-            int east = first.x + 1;
-            int y = first.y;
-            List<Point> points = List.of(new Point(y - 1, east), new Point(y, east), new Point(y + 1, east));
-            return elves.stream().map(e -> e.first).noneMatch(points::contains);
+        boolean directionIsEmpty(Direction direction, Set<Point> points) {
+            return direction.getPoints(current).noneMatch(points::contains);
         }
     }
 
