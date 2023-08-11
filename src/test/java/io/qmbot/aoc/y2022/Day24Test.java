18cb5a5a27b85045c756b2153105dca247651d54
QmBotox <122793467+QmBotox@users.noreply.github.com>
2023-08-11 21:46:45 +0300

    Day24 (#35)
    * Day04
    
    * .gitignore
    
    * Day23
    
    * Day24

---
+++ src/test/java/io/qmbot/aoc/y2022/Day24Test.java
@@ -0,0 +1,25 @@
+package io.qmbot.aoc.y2022;
+
+import io.qmbot.aoc.Puzzle;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Day24Test {
+    Puzzle p = new Day24();
+    String input = "#.######\n" +
+            "#>>.<^<#\n" +
+            "#.<..<<#\n" +
+            "#>v.><>#\n" +
+            "#<^v^^>#\n" +
+            "######.#";
+
+    @Test
+    public void part1() {
+        Assertions.assertEquals("18", p.part1(input));
+    }
+
+    @Test
+    public void part2() {
+        Assertions.assertEquals("54", p.part2(input));
+    }
+}
