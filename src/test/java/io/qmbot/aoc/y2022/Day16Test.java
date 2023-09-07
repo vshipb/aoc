d9faf15aee103d9d9881dc260cf892b53e06e195
QmBotox <122793467+QmBotox@users.noreply.github.com>
2023-09-07 11:11:49 +0300

    Day16 (#10)
    * Day04
    
    * Day02Test
    
    * Day03Test
    
    * Day16
    
    * .gitignore
    
    * Day23
    
    * Day16
    
    * Day16

---
+++ src/test/java/io/qmbot/aoc/y2022/Day16Test.java
@@ -0,0 +1,30 @@
+package io.qmbot.aoc.y2022;
+
+import io.qmbot.aoc.Puzzle;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Day16Test {
+    Puzzle p = new Day16();
+
+    String input = "Valve AA has flow rate=0; tunnels lead to valves DD, II, BB\n" +
+            "Valve BB has flow rate=13; tunnels lead to valves CC, AA\n" +
+            "Valve CC has flow rate=2; tunnels lead to valves DD, BB\n" +
+            "Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE\n" +
+            "Valve EE has flow rate=3; tunnels lead to valves FF, DD\n" +
+            "Valve FF has flow rate=0; tunnels lead to valves EE, GG\n" +
+            "Valve GG has flow rate=0; tunnels lead to valves FF, HH\n" +
+            "Valve HH has flow rate=22; tunnel leads to valve GG\n" +
+            "Valve II has flow rate=0; tunnels lead to valves AA, JJ\n" +
+            "Valve JJ has flow rate=21; tunnel leads to valve II";
+
+    @Test
+    public void part1(){
+        Assertions.assertEquals("1651", p.part1(input));
+    }
+
+    @Test
+    public void part2(){
+        Assertions.assertEquals("1707", p.part2(input));
+    }
+}
