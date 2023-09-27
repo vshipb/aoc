707b31443e7e3899db53181ab3aa0d965309a151
QmBotox <122793467+QmBotox@users.noreply.github.com>
2023-09-27 10:49:53 +0300

    Update (#38)

--- src/main/java/io/qmbot/aoc/y2022/Day02.java
+++ src/main/java/io/qmbot/aoc/y2022/Day02.java
@@ -1,51 +1,38 @@
 package io.qmbot.aoc.y2022;
 
 import io.qmbot.aoc.Puzzle;
+import java.util.Arrays;
 
 public class Day02 implements Puzzle {
     @Override
     public String part1(String input) {
-        int count = 0;
-        for (String round : input.split("\n")) {
-            if (round.contains("X")) {
-                count++;
-            }
-            if (round.contains("Y")) {
-                count = count + 2;
-            }
-            if (round.contains("Z")) {
-                count = count + 3;
-            }
-            if (((round.contains("A X")) || (round.contains("B Y")) || (round.contains("C Z")))) {
-                count = count + 3;
-            }
-            if (((round.contains("A Y")) || (round.contains("B Z")) || (round.contains("C X")))) {
-                count = count + 6;
-            }
-        }
-        return String.valueOf(count);
+        return String.valueOf(Arrays.stream(input.split("\n")).mapToInt(round -> switch (round) {
+            case "A X" -> 1 + 3;
+            case "A Y" -> 2 + 6;
+            case "A Z" -> 3;
+            case "B X" -> 1;
+            case "B Y" -> 2 + 3;
+            case "B Z" -> 3 + 6;
+            case "C X" -> 1 + 6;
+            case "C Y" -> 2;
+            case "C Z" -> 3 + 3;
+            default -> throw new IllegalArgumentException();
+        }).sum());
     }
 
     @Override
     public String part2(String input) {
-        int count = 0;
-        for (String round : input.split("\n")) {
-            if (round.contains("Y")) {
-                count = count + 3;
-            }
-            if (round.contains("Z")) {
-                count = count + 6;
-            }
-            if (((round.contains("A X")) || (round.contains("B Z")) || (round.contains("C Y")))) {
-                count = count + 3;
-            }
-            if (((round.contains("A Z")) || (round.contains("B Y")) || (round.contains("C X")))) {
-                count = count + 2;
-            }
-            if (((round.contains("A Y")) || (round.contains("B X")) || (round.contains("C Z")))) {
-                count = count + 1;
-            }
-        }
-        return String.valueOf(count);
+        return String.valueOf(Arrays.stream(input.split("\n")).mapToInt(round -> switch (round) {
+            case "A X" -> 3;
+            case "A Y" -> 1 + 3;
+            case "A Z" -> 2 + 6;
+            case "B X" -> 1;
+            case "B Y" -> 2 + 3;
+            case "B Z" -> 3 + 6;
+            case "C X" -> 2;
+            case "C Y" -> 3 + 3;
+            case "C Z" -> 1 + 6;
+            default -> throw new IllegalArgumentException();
+        }).sum());
     }
 }
