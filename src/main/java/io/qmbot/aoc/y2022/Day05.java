e4ddbb74914c8a2a1d91ba288b96d3995f897bfe
QmBotox <arina.shorina@gmail.com>
2023-10-04 11:22:07 +0300

    Update_5

--- src/main/java/io/qmbot/aoc/y2022/Day05.java
+++ src/main/java/io/qmbot/aoc/y2022/Day05.java
@@ -8,9 +8,9 @@ import java.util.List;
 public class Day05 implements Puzzle {
     @Override
     public String part1(String input) {
-        String[] splitted = input.split(REGEX_NEW_LINE + REGEX_NEW_LINE);
-        List<LinkedList<Character>> stacksOfCrates = stacksOfCrates(splitted[0].lines().toList());
-        crateMover9000(stacksOfCrates, splitted[1].lines().toList(), ((lines, from, to, count) -> {
+        String[] split = input.split(REGEX_EMPTY_LINE);
+        List<LinkedList<Character>> stacksOfCrates = stacksOfCrates(split[0].lines().toList());
+        move(stacksOfCrates, split[1].lines().toList(), ((lines, from, to, count) -> {
             for (int i = 0; i < count; i++) {
                 lines.get(to).push(lines.get(from).pop());
             }
@@ -20,16 +20,14 @@ public class Day05 implements Puzzle {
 
     @Override
     public String part2(String input) {
-        String[] splitted = input.split(REGEX_NEW_LINE + REGEX_NEW_LINE);
-        List<LinkedList<Character>> stacksOfCrates = stacksOfCrates(splitted[0].lines().toList());
-        crateMover9001(stacksOfCrates, splitted[1].lines().toList(), ((lines, from, to, count) -> {
+        String[] split = input.split(REGEX_EMPTY_LINE);
+        List<LinkedList<Character>> stacksOfCrates = stacksOfCrates(split[0].lines().toList());
+        move(stacksOfCrates, split[1].lines().toList(), ((lines, from, to, count) -> {
             LinkedList<Character> reserve = new LinkedList<>();
             for (int i = 0; i < count; i++) {
                 reserve.push(lines.get(from).pop());
             }
-            for (int i = 0; i < count; i++) {
-                lines.get(to).push(reserve.pop());
-            }
+            reserve.forEach(r -> lines.get(to).push(r));
         }));
         return result(stacksOfCrates);
     }
@@ -43,7 +41,7 @@ public class Day05 implements Puzzle {
     }
 
     private static List<LinkedList<Character>> stacksOfCrates(List<String> strings) {
-        String stringWithStacks = strings.get(strings.size() - 1).replaceAll(" ", "");
+        String stringWithStacks = strings.get(strings.size() - 1).trim();
         int countOfStacks = Character.getNumericValue(stringWithStacks.charAt(stringWithStacks.length() - 1));
         List<LinkedList<Character>> stacks = new ArrayList<>();
         for (int i = 0; i < countOfStacks; i++) {
@@ -59,17 +57,7 @@ public class Day05 implements Puzzle {
         return stacks;
     }
 
-    private static void crateMover9000(List<LinkedList<Character>> lines, List<String> moves, Mover mover) {
-        for (String string : moves) {
-            String[] stringSplit = string.split(" ");
-            int count = Integer.parseInt(stringSplit[1]);
-            int from = Integer.parseInt(stringSplit[3]) - 1;
-            int to = Integer.parseInt(stringSplit[5]) - 1;
-            mover.move(lines, from, to, count);
-        }
-    }
-
-    private static void crateMover9001(List<LinkedList<Character>> lines, List<String> moves, Mover mover) {
+    private static void move(List<LinkedList<Character>> lines, List<String> moves, Mover mover) {
         for (String string : moves) {
             String[] stringSplit = string.split(" ");
             int count = Integer.parseInt(stringSplit[1]);
