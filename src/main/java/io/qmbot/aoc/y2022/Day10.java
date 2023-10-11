03ccb7e1afb077f9a8b5a1a88bbf716fe2c7bf2a
QmBotox <arina.shorina@gmail.com>
2023-10-11 14:11:40 +0300

    Update_10

--- src/main/java/io/qmbot/aoc/y2022/Day10.java
+++ src/main/java/io/qmbot/aoc/y2022/Day10.java
@@ -1,62 +1,49 @@
 package io.qmbot.aoc.y2022;
 
 import io.qmbot.aoc.Puzzle;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Day10 implements Puzzle {
     @Override
     public String part1(String input) {
-        List<String> commands = new ArrayList<>();
-        for (String string : input.split("\n")) {
-            String[] split = string.split(" ");
-            commands.addAll(Arrays.asList(split));
-        }
+        List<String> commands = Arrays.stream(input.split(REGEX_NEW_LINE)).flatMap(line -> Arrays.stream(line.split(" "))).toList();
         int x = 1;
         int cycles = 0;
         int signals = 0;
-        for (String string : commands) {
+        for (String cmd : commands) {
             cycles++;
             if ((cycles - 20) % 40 == 0) {
-                signals = signals + cycles * x;
+                signals += cycles * x;
             }
             if (cycles == 220) {
                 break;
             }
-            if (!string.equals("noop") && !string.equals("addx")) {
-                x = x + Integer.parseInt(string);
-            }
+            x = addX(x, cmd);
         }
         return Integer.toString(signals);
     }
 
     @Override
     public String part2(String input) {
-        List<String> commands = new ArrayList<>();
-        for (String string : input.split("\n")) {
-            String[] split = string.split(" ");
-            commands.addAll(Arrays.asList(split));
-        }
+        List<String> commands = Arrays.stream(input.split(REGEX_NEW_LINE)).flatMap(line -> Arrays.stream(line.split(" "))).toList();
         StringBuilder string = new StringBuilder();
         int spriteX = 1;
         for (int y = 0; y < 6; y++) {
             for (int x = 0; x < 40; x++) {
-                int index = y * 40 + x;
-                if (Math.abs(spriteX - x) <= 1) {
-                    string.append("#");
-                } else {
-                    string.append(".");
-                }
-                String cmd = commands.get(index);
-                if (!cmd.equals("noop") && !cmd.equals("addx")) {
-                    spriteX = spriteX + Integer.parseInt(cmd);
-                }
+                string.append(Math.abs(spriteX - x) <= 1 ? "#" : ".");
+                spriteX = addX(spriteX, commands.get(y * 40 + x));
             }
-            string.append("\n");
+            string.append(REGEX_NEW_LINE);
         }
-        int endOfString = string.length();
-        string.delete(endOfString - 1, endOfString);
+        string.setLength(string.length() - 1);
         return string.toString();
+    }
+
+    private static int addX(int x, String cmd) {
+        if (!cmd.equals("noop") && !cmd.equals("addx")) {
+            return x + Integer.parseInt(cmd);
+        }
+        return x;
     }
 }
