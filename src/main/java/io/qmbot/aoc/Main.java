e5ffbed80a25aa9d820d54e928683a469aa2d2d7
QmBotox <arina.shorina@gmail.com>
2023-10-27 11:51:59 +0300

    Main upd

--- src/main/java/io/qmbot/aoc/Main.java
+++ src/main/java/io/qmbot/aoc/Main.java
@@ -11,44 +11,44 @@ import okhttp3.Response;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        final String COOKIE = args[0];
-        final String NAME = args[1];
-        final int YEAR = Integer.parseInt(args[2]);
+        final String cookie = args[0];
+        final String name = args[1];
+        final int year = Integer.parseInt(args[2]);
         if (args.length < 4) {
             for (int i = 1; i < 26; i++){
-                doDay(i, YEAR, NAME, COOKIE);
+                doDay(i, year, name, cookie);
             }
         } else {
             final int DAY = Integer.parseInt(args[3]);
-            doDay(DAY, YEAR, NAME, COOKIE);
+            doDay(DAY, year, name, cookie);
         }
     }
 
-    private static void doDay(int DAY, int YEAR, String NAME, String FOLDER) throws Exception {
-        String formatted = String.format("io.qmbot.aoc.y%d.Day%02d", YEAR, DAY);
+    private static void doDay(int day, int year, String name, String folder) throws Exception {
+        String formatted = String.format("io.qmbot.aoc.y%d.Day%02d", year, day);
         Puzzle p = (Puzzle) Class.forName(formatted).getDeclaredConstructor().newInstance();
-        File file = new File(String.format("src/main/resources/%s/%d/%d.txt", NAME, YEAR, DAY));
+        File file = new File(String.format("src/main/resources/%s/%d/%d.txt", name, year, day));
         if (!file.exists()) {
-            writeFile(DAY, YEAR, NAME, FOLDER);
+            writeFile(day, year, name, folder);
         }
         String input = Files.readString(Path.of(file.getPath()));
-        System.out.printf("Day %d answer%n%s%n%s%n%n", DAY, p.part1(input), p.part2(input));
+        System.out.printf("Day %d answer%n%s%n%s%n%n", day, p.part1(input), p.part2(input));
 
     }
 
-    private static void writeFile(int DAY, int YEAR, String NAME, String COOKIE) throws IOException {
-        String url = String.format("https://adventofcode.com/%d/day/%d/input", YEAR, DAY);
-        Request request = new Request.Builder().url(url).addHeader("Cookie", COOKIE).build();
+    private static void writeFile(int day, int year, String name, String cookie) throws IOException {
+        String url = String.format("https://adventofcode.com/%d/day/%d/input", year, day);
+        Request request = new Request.Builder().url(url).addHeader("Cookie", cookie).build();
         OkHttpClient client = new OkHttpClient().newBuilder().build();
         Response response = client.newCall(request).execute();
         if (!response.isSuccessful()) {
             throw new IllegalArgumentException(String.valueOf(response));
         }
-        String folder = String.format("src/main/resources/%s/%d", NAME, YEAR);
+        String folder = String.format("src/main/resources/%s/%d", name, year);
         File file = new File(folder);
         if (!file.exists()) {
             file.mkdirs();
         }
-        Files.write(Path.of(folder, DAY + ".txt"), Objects.requireNonNull(response.body()).string().getBytes());
+        Files.write(Path.of(folder, day + ".txt"), Objects.requireNonNull(response.body()).string().getBytes());
     }
 }
