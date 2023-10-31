43792ebe03a4929a298302c199a0b0a8cea603e8
QmBotox <arina.shorina@gmail.com>
2023-10-31 10:26:38 +0200

    Upd main

--- src/main/java/io/qmbot/aoc/Main.java
+++ src/main/java/io/qmbot/aoc/Main.java
@@ -1,6 +1,5 @@
 package io.qmbot.aoc;
 
-import java.io.File;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URI;
@@ -13,32 +12,32 @@ import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        final String cookie = args[0];
-        final String name = args[1];
-        final int year = Integer.parseInt(args[2]);
+        String dir = args[0];
+        int year = Integer.parseInt(args[1]);
+        String cookie = args[2];
         if (args.length < 4) {
             for (int i = 1; i < 26; i++) {
-                doDay(i, year, name, cookie);
+                doDay(dir, year, i, cookie);
             }
         } else {
-            final int DAY = Integer.parseInt(args[3]);
-            doDay(DAY, year, name, cookie);
+            int day = Integer.parseInt(args[3]);
+            doDay(dir, year, day, cookie);
         }
     }
 
-    private static void doDay(int day, int year, String name, String folder) throws Exception {
+    private static void doDay(String dir, int year, int day, String cookie) throws Exception {
         String formatted = String.format("io.qmbot.aoc.y%d.Day%02d", year, day);
         Puzzle p = (Puzzle) Class.forName(formatted).getDeclaredConstructor().newInstance();
-        File file = new File(String.format("src/main/resources/%s/%d/%d.txt", name, year, day));
-        if (!file.exists()) {
-            writeFile(day, year, name, folder);
-        }
-        String input = Files.readString(Path.of(file.getPath()));
+        String input = getInput(dir, year, day, cookie);
         System.out.printf("Day %d answer%n%s%n%s%n%n", day, p.part1(input), p.part2(input));
-
     }
 
-    private static void writeFile(int day, int year, String name, String cookie) throws IOException, InterruptedException {
+    private static String getInput(String dir, int year, int day, String cookie) throws IOException, InterruptedException {
+        Path path = Path.of(String.format("%s/%d/%d.txt", dir, year, day));
+        return (Files.exists(path)) ? Files.readString(path) : fetchInput(path, year, day, cookie);
+    }
+
+    private static String fetchInput(Path path, int year, int day, String cookie) throws IOException, InterruptedException {
         String url = String.format("https://adventofcode.com/%d/day/%d/input", year, day);
         HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).header("Cookie", cookie).build();
         HttpClient client = HttpClient.newHttpClient();
@@ -46,11 +45,9 @@ public class Main {
         if (response.statusCode() != HttpURLConnection.HTTP_OK) {
             throw new IllegalArgumentException(String.valueOf(response));
         }
-        String folder = String.format("src/main/resources/%s/%d", name, year);
-        File file = new File(folder);
-        if (!file.exists()) {
-            file.mkdirs();
-        }
-        Files.write(Path.of(folder, day + ".txt"), Objects.requireNonNull(response.body()).getBytes());
+        Files.createDirectories(path.getParent());
+        String input = response.body();
+        Files.write(path, Objects.requireNonNull(input).getBytes());
+        return input;
     }
 }
