c160627f4030c8aedecb04461bce323f7b249335
QmBotox <arina.shorina@gmail.com>
2023-11-09 15:45:48 +0200

    D19

--- src/main/java/io/qmbot/aoc/y2020/Day19.java
+++ src/main/java/io/qmbot/aoc/y2020/Day19.java
@@ -3,7 +3,6 @@ package io.qmbot.aoc.y2020;
 import io.qmbot.aoc.Puzzle;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -15,8 +14,11 @@ public class Day19 implements Puzzle {
         String[] parts = input.split(REGEX_EMPTY_LINE);
         String[] rules = parts[0].split(REGEX_NEW_LINE);
         Map<Integer, Rule> allRules = new HashMap<>();
-        Map<Integer, String> base = base(rules);
-        String regex = regex(0, allRules, base);
+        for (String rule : rules){
+            String[] r = rule.split(": ");
+            allRules.put(Integer.valueOf(r[0]), parseRule(r[1], allRules));
+        }
+        String regex = allRules.get(0).regex();
         int result = 0;
         for (String s : parts[1].split(REGEX_NEW_LINE)) {
             if (Pattern.matches(regex, s)) {
@@ -31,10 +33,16 @@ public class Day19 implements Puzzle {
         String[] parts = input.split(REGEX_EMPTY_LINE);
         String[] rules = parts[0].split(REGEX_NEW_LINE);
         Map<Integer, Rule> allRules = new HashMap<>();
-        Map<Integer, String> base = base(rules);
-        base.put(8, " 42 | 42 8");
-        base.put(11, " 42 31 | 42 11 31");
-        String regex = regex(0, allRules, base);
+        for (String rule : rules){
+            String[] r = rule.split(": ");
+            allRules.put(Integer.valueOf(r[0]), parseRule(r[1], allRules));
+        }
+        allRules.put(8, new Rule8(new RefRule(42, allRules)));
+        allRules.put(11, new Rule11(new RefRule(42, allRules), new RefRule(31, allRules)));
+//        Map<Integer, String> base = base(rules);
+//        base.put(8, " 42 | 42 8");
+//        base.put(11, " 42 31 | 42 11 31");
+        String regex = allRules.get(0).regex();
         int result = 0;
         for (String s : parts[1].split(REGEX_NEW_LINE)) {
             if (Pattern.matches(regex, s)) {
@@ -44,96 +52,26 @@ public class Day19 implements Puzzle {
         return String.valueOf(result);
     }
 
-    private static String regex(int number, Map<Integer, Rule> allRules, Map<Integer, String> base) {
-        addRule(number, allRules, base);
-        Rule currentRule = allRules.get(number);
-        return currentRule.toString();
-    }
-
-    Rule parseRule(String str, Map<Integer, Rule> allRules) {
-        String[] split = str.split(" ");
-        int number = Integer.parseInt(split[0].substring(0, split[0].length() - 1));
-        if (number == 8) {
-            return new Rule8(split, allRules, base);
-        } else if (number == 11) {
-            return new Rule11(split, allRules, base);
-        } else if (split[1].contains("\"")) {
-            return new CharRule(split[1].charAt(1));
+    static Rule parseRule(String str, Map<Integer, Rule> allRules) {
+        if (str.contains("|")) {
+            String[] split = str.split("\\|", 2);
+            return new OrRule(parseRule(split[0].trim(), allRules), parseRule(split[1].trim(), allRules));
+        } else if (str.contains(" ")) {
+            String[] split = str.split(" ", 2);
+            return new SequenceRule(parseRule(split[0].trim(), allRules), parseRule(split[1].trim(), allRules));
+        } else if (str.contains("\"")){
+            return new CharRule(str.charAt(1));
         } else {
-            int index = Arrays.asList(split).indexOf("|");
-            if (index > 0) {
-                String[] splitted = str.split("|");
-                return new OrRule(parseRule(splitted[0].trim(), allRules), parseRule(splitted[1].trim(), allRules));
-            } else {
-                return new SequenceRule(split, allRules, base);
-            }
+            return new RefRule(Integer.parseInt(str), allRules);
         }
-    }
-
-//    private static void addRule(int number, Map<Integer, Rule> allRules, Map<Integer, String> base) {
-//        String[] split = base.get(number).split(" ");
-//        if (number == 8) {
-//            Rule8 rule = new Rule8(split, allRules, base);
-//            allRules.put(number, rule);
-//        } else if (number == 11) {
-//            Rule11 rule = new Rule11(split, allRules, base);
-//            allRules.put(number, rule);
-//        } else if (split[1].contains("\"")) {
-//            CharRule rule = new CharRule(split[1].charAt(1));
-//            allRules.put(number, rule);
-//        } else {
-//            int index = Arrays.asList(split).indexOf("|");
-//            if (index > 0) {
-//                OrRule rule = new OrRule(index, split, allRules, base);
-//                allRules.put(number, rule);
-//            } else {
-//                SequenceRule rule = new SequenceRule(split, allRules, base);
-//                allRules.put(number, rule);
-//            }
-//        }
-//    }
-
-    private static Map<Integer, String> base(String[] rules) {
-        Map<Integer, String> base = new HashMap<>();
-        for (String string : rules) {
-            String[] rule = string.split(":");
-            base.put(Integer.valueOf(rule[0]), rule[1]);
-        }
-        return base;
-    }
-
-    private static List<Integer> listNumbers(String[] split, int start, int end) {
-        List<Integer> listNumbers = new ArrayList<>();
-        for (int i = start; i < end; i++) {
-            listNumbers.add(Integer.valueOf(split[i]));
-        }
-        return listNumbers;
-    }
-    static String buildStringFromRules(List<Rule> rules) {
-        StringBuilder stringBuilder = new StringBuilder();
-        for (Rule rule : rules) {
-            stringBuilder.append(rule.toString());
-        }
-        return String.valueOf(stringBuilder);
     }
 
     private abstract static class Rule {
 
         abstract String regex();
-
-         static List<Rule> listRules(List<Integer> numbers, Map<Integer, Rule> allRules, Map<Integer, String> base) {
-            List<Rule> listRules = new ArrayList<>();
-            for (int number : numbers) {
-                if (!allRules.containsKey(number)) {
-                    addRule(number, allRules, base);
-                }
-                listRules.add(allRules.get(number));
-            }
-            return listRules;
-        }
     }
 
-    static class RefRule extends Rule{
+    static class RefRule extends Rule {
         int number;
         Map<Integer, Rule> allRules;
 
@@ -150,26 +88,29 @@ public class Day19 implements Puzzle {
 
     static class CharRule extends Rule {
         char c;
+
         public CharRule(char c) {
             this.c = c;
         }
 
         @Override
         public String regex() {
-            return String.valueOf(c);
+            return "(" + c + ")";
         }
     }
 
     static class SequenceRule extends Rule {
         Rule first;
         Rule second;
-        public SequenceRule(String[] split, Map<Integer, Rule> allRules, Map<Integer, String> base) {
 
+        public SequenceRule(Rule first, Rule second) {
+            this.first = first;
+            this.second = second;
         }
 
         @Override
         public String regex() {
-            return first.regex() + second.regex();
+            return "(" + first.regex() + second.regex() + ")";
         }
     }
 
@@ -189,40 +130,31 @@ public class Day19 implements Puzzle {
     }
 
     static class Rule8 extends Rule {
-        List<Rule> rules;
-        public void setRules(List<Integer> rules, Map<Integer, Rule> allRules, Map<Integer, String> base) {
-            this.rules = listRules(rules, allRules, base);
-        }
-        public Rule8(String[] split, Map<Integer, Rule> allRules, Map<Integer, String> base) {
-            setRules(listNumbers(split, 1, 2), allRules, base);
+        Rule rule;
+
+        public Rule8(Rule rule) {
+            this.rule = rule;
         }
 
         @Override
         public String regex() {
-            return "(" + buildStringFromRules(rules) + ")+";
+            return "(" + rule.regex() + ")+";
         }
     }
 
     static class Rule11 extends Rule {
-        List<Rule> rules;
-        public void setRules(List<Integer> rules, Map<Integer, Rule> allRules, Map<Integer, String> base) {
-            this.rules = listRules(rules, allRules, base);
+        Rule firstRule;
+        Rule secondRule;
+
+        public Rule11(Rule firstRule, Rule secondRule) {
+            this.firstRule = firstRule;
+            this.secondRule = secondRule;
         }
-        public Rule11(String[] split, Map<Integer, Rule> allRules, Map<Integer, String> base) {
-            setRules(listNumbers(split, 1, 3), allRules, base);
-        }
+
         @Override
         public String regex() {
-            return "(" + (rules.get(0)) + "{1}" + (rules.get(1)) + "{1})|(" +
-                    (rules.get(0)) + "{2}" + (rules.get(1)) + "{2})|(" +
-                    (rules.get(0)) + "{3}" + (rules.get(1)) + "{3})|(" +
-                    (rules.get(0)) + "{4}" + (rules.get(1)) + "{4})|(" +
-                    (rules.get(0)) + "{5}" + (rules.get(1)) + "{5})|(" +
-                    (rules.get(0)) + "{6}" + (rules.get(1)) + "{6})|(" +
-                    (rules.get(0)) + "{7}" + (rules.get(1)) + "{7})|(" +
-                    (rules.get(0)) + "{8}" + (rules.get(1)) + "{8})|(" +
-                    (rules.get(0)) + "{9}" + (rules.get(1)) + "{9})|(" +
-                    (rules.get(0)) + "{10}" + (rules.get(1)) + "{10})";
+            return "(" + firstRule.regex() + "{1}" + secondRule.regex() + "{1})|(" +
+                    firstRule.regex() + "{10}" + secondRule.regex() + "{10})";
         }
     }
 }
