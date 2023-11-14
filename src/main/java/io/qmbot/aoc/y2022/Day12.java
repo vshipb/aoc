1e025e46957e9359b10e52ceed7a58239d648194
QmBotox <arina.shorina@gmail.com>
2023-11-14 15:15:50 +0200

    D12_upd

--- src/main/java/io/qmbot/aoc/y2022/Day12.java
+++ src/main/java/io/qmbot/aoc/y2022/Day12.java
@@ -3,115 +3,87 @@ package io.qmbot.aoc.y2022;
 import io.qmbot.aoc.Puzzle;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.function.BiFunction;
 
 public class Day12 implements Puzzle {
     @Override
     public Integer part1(String input) {
-        List<String> strings = List.of(input.split("\n"));
-        char[][] charField = new char[strings.size()][strings.get(0).length()];
-        int[][] stepField = new int[strings.size()][strings.get(0).length()];
-        Point startPoint = new Point(0, 0);
-        Point endPoint = new Point(0, 0);
-        for (int j = 0; j < strings.size(); j++) {
-            String string = strings.get(j);
-            for (int i = 0; i < string.length(); i++) {
-                charField[j][i] = string.charAt(i);
-                if (charField[j][i] == 'S') {
-                    charField[j][i] = 'a';
-                    startPoint = new Point(i, j);
-                    stepField[j][i] = 0;
-                }
-                if (charField[j][i] == 'E') {
-                    charField[j][i] = 'z';
-                    endPoint = new Point(i, j);
-                }
-            }
-        }
+        List<String> strings = List.of(input.split(REGEX_NEW_LINE));
+        int stringSize = strings.size();
+        int stringLength = strings.get(0).length();
+        char[][] charField = new char[stringSize][stringLength];
+        int[][] stepField = new int[stringSize][stringLength];
+        List<Point> startAndEndPoints = startAndEndPoints(strings, charField);
+        Point startPoint = startAndEndPoints.get(0);
+        Point endPoint = startAndEndPoints.get(1);
         List<Point> points = List.of(new Point(startPoint.x, startPoint.y));
-        int neighborCounts = 1;
-        while (neighborCounts != 0) {
-            points = fromBottomToTopCheck(charField, stepField, points);
-            neighborCounts = points.size();
+        while (true) {
+            points = check(charField, stepField, points,
+                    (Point point, Point neighbor) -> charField[point.y][point.x] + 1 >=  charField[neighbor.y][neighbor.x]);
+            if (stepField[endPoint.y][endPoint.x] > 0)
+                return stepField[endPoint.y][endPoint.x];
         }
-        return stepField[endPoint.y][endPoint.x];
     }
 
     @Override
     public Integer part2(String input) {
-        List<String> strings = List.of(input.split("\n"));
+        List<String> strings = List.of(input.split(REGEX_NEW_LINE));
         int stringSize = strings.size();
-        int stringSize1 = strings.get(0).length();
-        char[][] charField = new char[stringSize][stringSize1];
-        int[][] stepField = new int[stringSize][stringSize1];
-        Point endPoint = new Point(0, 0);
+        int stringLength = strings.get(0).length();
+        char[][] charField = new char[stringSize][stringLength];
+        int[][] stepField = new int[stringSize][stringLength];
+        Point endPoint = startAndEndPoints(strings, charField).get(1);
+        List<Point> points = List.of(new Point(endPoint.x, endPoint.y));
+        while (true) {
+            points = check(charField, stepField, points,
+                    (Point point, Point neighbor) -> charField[point.y][point.x] - 1 <= charField[neighbor.y][neighbor.x]);
+            for (Point point : points) {
+                if (charField[point.y][point.x] == 'a')
+                    return stepField[point.y][point.x];
+            }
+        }
+    }
+
+    List<Point> startAndEndPoints(List<String> strings, char[][] charField) {
+        Point startPoint = null; Point endPoint = null;
         for (int j = 0; j < strings.size(); j++) {
             String string = strings.get(j);
             for (int i = 0; i < string.length(); i++) {
-                charField[j][i] = string.charAt(i);
-                if (charField[j][i] == 'S') {
+                char currentChar = string.charAt(i);
+                charField[j][i] = currentChar;
+                if (currentChar == 'S') {
                     charField[j][i] = 'a';
-                }
-                if (charField[j][i] == 'E') {
+                    startPoint = new Point(i, j);
+                } else if (currentChar == 'E') {
                     charField[j][i] = 'z';
                     endPoint = new Point(i, j);
                 }
             }
         }
-        List<Point> points = List.of(new Point(endPoint.x, endPoint.y));
-        while (charField[points.get(0).y][points.get(0).x] != 'a') {
-            points = fromTopToBottomCheck(charField, stepField, points);
-        }
-        return stepField[points.get(0).y][points.get(0).x];
+        return List.of(Objects.requireNonNull(startPoint), Objects.requireNonNull(endPoint));
     }
 
     record Point(int x, int y) {
 
         List<Point> neighbors(int sizeX, int sizeY) {
             List<Point> neighbors = new ArrayList<>();
-            if (x > 0) {
-                neighbors.add(new Point(x - 1, y));
-            }
-            if (x < sizeX - 1) {
-                neighbors.add(new Point(x + 1, y));
-            }
-            if (y > 0) {
-                neighbors.add(new Point(x, y - 1));
-            }
-            if (y < sizeY - 1) {
-                neighbors.add(new Point(x, y + 1));
-            }
+            if (x > 0) neighbors.add(new Point(x - 1, y));
+            if (x < sizeX - 1) neighbors.add(new Point(x + 1, y));
+            if (y > 0) neighbors.add(new Point(x, y - 1));
+            if (y < sizeY - 1) neighbors.add(new Point(x, y + 1));
             return neighbors;
         }
 
     }
-
-    private static List<Point> fromBottomToTopCheck(char[][] charField, int[][] stepField, List<Point> nowPoints) {
+    
+    private static List<Point> check(char[][] charField, int[][] stepField, List<Point> nowPoints, BiFunction<Point, Point, Boolean> check) {
         List<Point> newPoints = new ArrayList<>();
         for (Point point : nowPoints) {
             for (Point neighbor : point.neighbors(charField[0].length, charField.length)) {
-                if (charField[point.y][point.x] + 1 >= charField[neighbor.y][neighbor.x]
-                        && stepField[neighbor.y][neighbor.x] == 0) {
+                if (check.apply(point, neighbor) && stepField[neighbor.y][neighbor.x] == 0) {
                     stepField[neighbor.y][neighbor.x] = stepField[point.y][point.x] + 1;
                     newPoints.add(neighbor);
-                }
-            }
-        }
-        return newPoints;
-    }
-
-    private static List<Point> fromTopToBottomCheck(char[][] charField, int[][] stepField, List<Point> nowPoints) {
-        List<Point> newPoints = new ArrayList<>();
-        for (Point point : nowPoints) {
-            for (Point neighbor : point.neighbors(charField[0].length, charField.length)) {
-                if (charField[point.y][point.x] - 1 <= charField[neighbor.y][neighbor.x]
-                        && stepField[neighbor.y][neighbor.x] == 0) {
-                    stepField[neighbor.y][neighbor.x] = stepField[point.y][point.x] + 1;
-                    newPoints.add(neighbor);
-                    if (charField[neighbor.y][neighbor.x] == 'a') {
-                        newPoints.clear();
-                        newPoints.add(neighbor);
-                        return newPoints;
-                    }
                 }
             }
         }
