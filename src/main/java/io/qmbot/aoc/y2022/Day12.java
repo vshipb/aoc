86f4fbb5a0968fdf0e13a371704e60d4b068afc2
QmBotox <arina.shorina@gmail.com>
2023-11-14 10:02:02 +0200

    D12_upd

--- src/main/java/io/qmbot/aoc/y2022/Day12.java
+++ src/main/java/io/qmbot/aoc/y2022/Day12.java
@@ -8,36 +8,32 @@ public class Day12 implements Puzzle {
     @Override
     public Integer part1(String input) {
         List<String> strings = List.of(input.split("\n"));
-        char[][] field = new char[strings.size()][strings.get(0).length()];
-        int[][] field1 = new int[strings.size()][strings.get(0).length()];
-        int startX = 0;
-        int startY = 0;
-        int endX = 0;
-        int endY = 0;
+        char[][] charField = new char[strings.size()][strings.get(0).length()];
+        int[][] stepField = new int[strings.size()][strings.get(0).length()];
+        Point startPoint = new Point(0, 0);
+        Point endPoint = new Point(0, 0);
         for (int j = 0; j < strings.size(); j++) {
             String string = strings.get(j);
             for (int i = 0; i < string.length(); i++) {
-                field[j][i] = string.charAt(i);
-                if (field[j][i] == 'S') {
-                    field[j][i] = 'a';
-                    startY = j;
-                    startX = i;
-                    field1[startY][startX] = 0;
+                charField[j][i] = string.charAt(i);
+                if (charField[j][i] == 'S') {
+                    charField[j][i] = 'a';
+                    startPoint = new Point(i, j);
+                    stepField[j][i] = 0;
                 }
-                if (field[j][i] == 'E') {
-                    field[j][i] = 'z';
-                    endX = i;
-                    endY = j;
+                if (charField[j][i] == 'E') {
+                    charField[j][i] = 'z';
+                    endPoint = new Point(i, j);
                 }
             }
         }
-        List<Point> points = List.of(new Point(startX, startY));
+        List<Point> points = List.of(new Point(startPoint.x, startPoint.y));
         int neighborCounts = 1;
         while (neighborCounts != 0) {
-            points = newPointsCheck(field, field1, points);
+            points = fromBottomToTopCheck(charField, stepField, points);
             neighborCounts = points.size();
         }
-        return field1[endY][endX];
+        return stepField[endPoint.y][endPoint.x];
     }
 
     @Override
@@ -45,29 +41,27 @@ public class Day12 implements Puzzle {
         List<String> strings = List.of(input.split("\n"));
         int stringSize = strings.size();
         int stringSize1 = strings.get(0).length();
-        char[][] field = new char[stringSize][stringSize1];
-        int[][] field1 = new int[stringSize][stringSize1];
-        int endX = 0;
-        int endY = 0;
+        char[][] charField = new char[stringSize][stringSize1];
+        int[][] stepField = new int[stringSize][stringSize1];
+        Point endPoint = new Point(0, 0);
         for (int j = 0; j < strings.size(); j++) {
             String string = strings.get(j);
             for (int i = 0; i < string.length(); i++) {
-                field[j][i] = string.charAt(i);
-                if (field[j][i] == 'S') {
-                    field[j][i] = 'a';
+                charField[j][i] = string.charAt(i);
+                if (charField[j][i] == 'S') {
+                    charField[j][i] = 'a';
                 }
-                if (field[j][i] == 'E') {
-                    field[j][i] = 'z';
-                    endX = i;
-                    endY = j;
+                if (charField[j][i] == 'E') {
+                    charField[j][i] = 'z';
+                    endPoint = new Point(i, j);
                 }
             }
         }
-        List<Point> points = List.of(new Point(endX, endY));
-        while (field[points.get(0).y][points.get(0).x] != 'a') {
-            points = newPointsCheck1(field, field1, points);
+        List<Point> points = List.of(new Point(endPoint.x, endPoint.y));
+        while (charField[points.get(0).y][points.get(0).x] != 'a') {
+            points = fromTopToBottomCheck(charField, stepField, points);
         }
-        return field1[points.get(0).y][points.get(0).x];
+        return stepField[points.get(0).y][points.get(0).x];
     }
 
     record Point(int x, int y) {
@@ -91,15 +85,13 @@ public class Day12 implements Puzzle {
 
     }
 
-    private static List<Point> newPointsCheck(char[][] field, int[][] field1, List<Point> nowPoints) {
+    private static List<Point> fromBottomToTopCheck(char[][] charField, int[][] stepField, List<Point> nowPoints) {
         List<Point> newPoints = new ArrayList<>();
-        List<Point> neighbors1;
         for (Point point : nowPoints) {
-            neighbors1 = point.neighbors(field[0].length, field.length);
-            for (Point neighbor : neighbors1) {
-                if (field[point.y][point.x] + 1 >= field[neighbor.y][neighbor.x]
-                        && field1[neighbor.y][neighbor.x] == 0) {
-                    field1[neighbor.y][neighbor.x] = field1[point.y][point.x] + 1;
+            for (Point neighbor : point.neighbors(charField[0].length, charField.length)) {
+                if (charField[point.y][point.x] + 1 >= charField[neighbor.y][neighbor.x]
+                        && stepField[neighbor.y][neighbor.x] == 0) {
+                    stepField[neighbor.y][neighbor.x] = stepField[point.y][point.x] + 1;
                     newPoints.add(neighbor);
                 }
             }
@@ -107,17 +99,15 @@ public class Day12 implements Puzzle {
         return newPoints;
     }
 
-    private static List<Point> newPointsCheck1(char[][] field, int[][] field1, List<Point> nowPoints) {
+    private static List<Point> fromTopToBottomCheck(char[][] charField, int[][] stepField, List<Point> nowPoints) {
         List<Point> newPoints = new ArrayList<>();
-        List<Point> neighbors1;
         for (Point point : nowPoints) {
-            neighbors1 = point.neighbors(field[0].length, field.length);
-            for (Point neighbor : neighbors1) {
-                if (field[point.y][point.x] - 1 <= field[neighbor.y][neighbor.x]
-                        && field1[neighbor.y][neighbor.x] == 0) {
-                    field1[neighbor.y][neighbor.x] = field1[point.y][point.x] + 1;
+            for (Point neighbor : point.neighbors(charField[0].length, charField.length)) {
+                if (charField[point.y][point.x] - 1 <= charField[neighbor.y][neighbor.x]
+                        && stepField[neighbor.y][neighbor.x] == 0) {
+                    stepField[neighbor.y][neighbor.x] = stepField[point.y][point.x] + 1;
                     newPoints.add(neighbor);
-                    if (field[neighbor.y][neighbor.x] == 'a') {
+                    if (charField[neighbor.y][neighbor.x] == 'a') {
                         newPoints.clear();
                         newPoints.add(neighbor);
                         return newPoints;
