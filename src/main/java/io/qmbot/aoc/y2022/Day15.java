63ace68b3016a9b3de86c29d92955a78d6601c1b
QmBotox <arina.shorina@gmail.com>
2023-11-23 12:51:30 +0200

    D15_upd

--- src/main/java/io/qmbot/aoc/y2022/Day15.java
+++ src/main/java/io/qmbot/aoc/y2022/Day15.java
@@ -1,109 +1,86 @@
 package io.qmbot.aoc.y2022;
 
 import io.qmbot.aoc.Puzzle;
-import org.jetbrains.annotations.NotNull;
-
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import org.jetbrains.annotations.NotNull;
 
 public class Day15 implements Puzzle {
     @Override
     public Long part1(String input) {
-        List<String> strings = List.of(input.split(REGEX_NEW_LINE));
-        int sensorX;
-        int sensorY;
-        int beaconX;
-        int beaconY;
         int min = 1500000;
-        int seekingY = min + 10;
-        List<Point> sensors = new ArrayList<>();
-        List<Point> beacons = new ArrayList<>();
-        int[] seekingField = new int[200000000];
-        for (String string : strings) {
-            String[] info = string.split(" ");
-            sensorX = Integer.parseInt(info[2].substring(2, info[2].length() - 1)) + min;
-            sensorY = Integer.parseInt(info[3].substring(2, info[3].length() - 1)) + min;
-            beaconX = Integer.parseInt(info[8].substring(2, info[8].length() - 1)) + min;
-            beaconY = Integer.parseInt(info[9].substring(2)) + min;
-            sensors.add(new Point(sensorY, sensorX));
-            beacons.add(new Point(beaconY, beaconX));
-            if (seekingY == (sensorY)) {
-                seekingField[sensorX] = 8;
-            }
-            if (seekingY == (beaconY)) {
-                seekingField[beaconX] = 6;
-            }
-        }
-        List<Segment> segments = new ArrayList<>();
-        Segment segment = new Segment(0, 0);
-        int max = 200000000;
-        for (Point point : sensors) {
-            Segment test = notFreeSpace(seekingY, point, beacons.get(sensors.indexOf(point)), max);
-            segments.add(test);
-        }
-        Collections.sort(segments);
-        for (Segment point : segments) {
-            segment = fuse(point, segment);
-        }
-        long result = segment.second - segment.first;
-        result = result - min;
-//        for (Point point : sensors) {
-//            sensorAreaAtString(seekingField, seekingY, point, beacons.get(sensors.indexOf(point)));
-//        }
-//        long freeSpace1 = 0;
-//        for (int j : seekingField) {
-//            if (j == 1) freeSpace1++;
-//        }
-        return result;
+        Field field = new Field(List.of(input.split(REGEX_NEW_LINE)), min);
+        field.sensorsAreaAtString(min + 10);
+        return Arrays.stream(field.seekingField).filter(value -> value == 1).count();
     }
 
     @Override
     public Long part2(String input) {
-        List<String> strings = List.of(input.split(REGEX_NEW_LINE));
-        int sensorX;
-        int sensorY;
-        int beaconX;
-        int beaconY;
-        int max = 4000000;
-        List<Point> sensors = new ArrayList<>();
-        List<Point> beacons = new ArrayList<>();
-        for (String string : strings) {
-            String[] info = string.split(" ");
-            sensorX = Integer.parseInt(info[2].substring(2, info[2].length() - 1));
-            sensorY = Integer.parseInt(info[3].substring(2, info[3].length() - 1));
-            beaconX = Integer.parseInt(info[8].substring(2, info[8].length() - 1));
-            beaconY = Integer.parseInt(info[9].substring(2));
-            sensors.add(new Point(sensorY, sensorX));
-            beacons.add(new Point(beaconY, beaconX));
-        }
-        int seekingX = 0;
-        List<Segment> segments = new ArrayList<>();
-        Segment segment;
-        Segment testing = new Segment(0, 0);
-        Segment falseField = new Segment(0, max);
-        for (int y = 0; y < max; y++) {
-            segments.clear();
-            segment = new Segment(0, 0);
-            for (Point point : sensors) {
-                Segment test = notFreeSpace(y, point, beacons.get(sensors.indexOf(point)), max);
-                if (!test.equals(testing)) {
-                    segments.add(test);
-                }
-            }
-            Collections.sort(segments);
-            for (Segment point : segments) {
-                segment = fuse(segment, point);
-                if (segment.equals(falseField)) break;
-                if (segment.equals(testing)) {
-                    return seekingX * 4000000L + y;
-                }
-                seekingX = segment.second + 1;
-            }
-        }
-        return null;
+        Field field = new Field(List.of(input.split(REGEX_NEW_LINE)), 0);
+        return field.resultPart2(4000000, 0);
     }
 
+    static class Field {
+        List<Point> sensors = new ArrayList<>();
+        List<Point> beacons = new ArrayList<>();
+        int[] seekingField = new int[200000000];
+
+        public Field(List<String> strings, int min) {
+            int sensorX;
+            int sensorY;
+            int beaconX;
+            int beaconY;
+            int seekingY = min + 10;
+            for (String string : strings) {
+                String[] info = string.split(" ");
+                sensorX = Integer.parseInt(info[2].substring(2, info[2].length() - 1)) + min;
+                sensorY = Integer.parseInt(info[3].substring(2, info[3].length() - 1)) + min;
+                beaconX = Integer.parseInt(info[8].substring(2, info[8].length() - 1)) + min;
+                beaconY = Integer.parseInt(info[9].substring(2)) + min;
+                this.sensors.add(new Point(sensorY, sensorX));
+                this.beacons.add(new Point(beaconY, beaconX));
+                if (seekingY == (sensorY)) {
+                    this.seekingField[sensorX] = 8;
+                }
+                if (seekingY == (beaconY)) {
+                    this.seekingField[beaconX] = 6;
+                }
+            }
+        }
+
+        void sensorsAreaAtString(int seekingY) {
+            sensors.forEach(point -> sensorAreaAtString(seekingField, seekingY, point, beacons.get(sensors.indexOf(point))));
+        }
+
+        long resultPart2(int max, int seekingX) {
+            List<Segment> segments = new ArrayList<>();
+            Segment segment;
+            Segment testing = new Segment(0, 0);
+            Segment falseField = new Segment(0, max);
+            for (int y = 0; y < max; y++) {
+                segments.clear();
+                segment = new Segment(0, 0);
+                for (Point point : sensors) {
+                    Segment test = notFreeSpace(y, point, beacons.get(sensors.indexOf(point)), max);
+                    if (!test.equals(testing)) {
+                        segments.add(test);
+                    }
+                }
+                Collections.sort(segments);
+                for (Segment point : segments) {
+                    segment = fuse(segment, point);
+                    if (segment.equals(falseField)) break;
+                    if (segment.equals(testing)) {
+                        return seekingX * 4000000L + y;
+                    }
+                    seekingX = segment.second + 1;
+                }
+            }
+            return 0L;
+        }
+    }
 
     static class Segment implements Comparable<Segment> {
         private final int first;
@@ -127,12 +104,6 @@ public class Day15 implements Puzzle {
             if (first != segment.first) return false;
             return second == segment.second;
         }
-
-        @Override
-        public String toString() {
-            return first +
-                    ", " + second;
-        }
     }
 
     static class Point {
@@ -152,8 +123,7 @@ public class Day15 implements Puzzle {
     private static void sensorAreaAtString(int[] field, int seekingY, Point sensor, Point beacon) {
         int taxicabDistance = Math.abs(sensor.partTwoX - beacon.partTwoX) + Math.abs(sensor.partOneY - beacon.partOneY);
         if (sensor.partOneY - taxicabDistance <= seekingY && seekingY <= sensor.partOneY + taxicabDistance) {
-            int x = Math.abs(seekingY - sensor.partOneY);
-            x = taxicabDistance - x;
+            int x = taxicabDistance - Math.abs(seekingY - sensor.partOneY);
             for (int i = sensor.partTwoX - x; i <= sensor.partTwoX + x; i++) {
                 if (field[i] == 0) field[i] = 1;
             }
